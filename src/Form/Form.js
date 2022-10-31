f44b2c86561d9d33bf9f138b9f327531f495aa5c
MatthewPress <press.matt14@gmail.com>
2022-10-31 10:46:26 -0600

    Feat: add event handler for user inputs

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -4,29 +4,49 @@ import './Form.css';
 
 function Form({}) {
   const [values, setValues] = useState({
-    name: '',
+    userName: '',
     numb: 0,
     date: '',
     time: ''
   });
+
+  const handleChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      [event.target.name]: event.target.value,
+    }));
+  }
 
   return (
     <form>
       <label>NAME</label>
       <input 
         type="text"
+        name="userName"
+        value={values.userName}
+        onChange={(event) => handleChange(event)}
       />
       <label>NUMBER OF PEEPS</label>
       <input 
         type="number"
+        name="numb"
+        value={values.numb}
+        onChange={(event) => handleChange(event)}
       />
       <label>DATE</label>
       <input 
         type="date"
+        name="date"
+        value={values.date}
+        onChange={(event) => handleChange(event)}
       />
       <label>TIME</label>
       <input 
         type="time"
+        name="time"
+        value={values.time}
+        onChange={(event) => handleChange(event)}
       />
       <input
         type="submit"
